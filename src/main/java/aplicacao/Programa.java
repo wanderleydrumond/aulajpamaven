c3734db670a61d47d24251ae2bf5a95e007f838a
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-01-15 17:22:11 +0000

    Inserir pessoas extraídos para um método próprio.

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,28 +1,35 @@
 package aplicacao;
 
 import dominio.Pessoa;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Programa {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa(null, "Carlos da Silva", "carlos@email.com");
-        Pessoa pessoa2 = new Pessoa(null, "Joaquim Torres", "joaquim@email.com");
-        Pessoa pessoa3 = new Pessoa(null, "Ana Maria", "ana@email.com");
-
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(pessoa1);
-        entityManager.persist(pessoa2);
-        entityManager.persist(pessoa3);
-        entityManager.getTransaction().commit();
+        inserirPessoas(entityManager);
 
-        System.out.println(pessoa1 + "\n"+ pessoa2 + "\n" + pessoa3 + "\n\nCadastrados com sucesso!");
         entityManager.close();
         entityManagerFactory.close();
+    }
+
+    private static void inserirPessoas(@NotNull EntityManager entityManager) {
+        Pessoa pessoa1 = new Pessoa(null, "Carlos da Silva", "carlos@email.com"),
+               pessoa2 = new Pessoa(null, "Joaquim Torres", "joaquim@email.com"),
+               pessoa3 = new Pessoa(null, "Ana Maria", "ana@email.com");
+        ArrayList<Pessoa> pessoas = new ArrayList<>(Arrays.asList(pessoa1, pessoa2, pessoa3));
+
+        entityManager.getTransaction().begin();
+        pessoas.forEach(entityManager::persist);
+        entityManager.getTransaction().commit();
+
+        pessoas.forEach(System.out::println);
     }
 }
